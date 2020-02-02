4e6cf3875ec2875f71a80acf70326a02aa116a96
DebarunMitra <debarun96mitra@gmail.com>
2020-02-02 12:55:49 +0530

    variable, string, operator, list

--- Basics/main.py
+++ Basics/main.py
@@ -1,7 +1,83 @@
-import math
+# import math
 
-print("hello world")
+# print("hello world")
 
-print(5+8)
+# print(5+8)
 
-print(math.gcd(3,6))
+# print(math.gcd(3,6))
+
+# operators
+# num1 = 7
+# num2 = 3
+# print(num1+num2)
+# print(num1-num2)
+# print(num1*num2)
+# print(num1/num2)
+# print(num1//2) #floor division operator
+# print(num1%num2) #modulo operator
+# print(num1**2) #Exponentiation
+# print(num1^num2) #XOR operator 
+
+
+# type casting
+# num = 1990
+# str_num=str(num)
+# print(type(num))
+# float_num=float(num)
+# print(type(float_num))
+# print(float_num)
+# print(type(str_num))
+# print(str_num)
+
+
+# multiline string
+# quote='''Ram,
+#     You are awesome'''
+# print(quote)
+
+# pattern='''
+#         *
+#        * *
+#       * * *
+#         8
+# '''
+# print(pattern)
+
+# string operation
+
+# harry = "Harry Potter"
+# db = "DragonBall Z"
+# print(harry[5:11]); #potter
+# print(harry.strip()) #cut the spaces before string
+# print(len(harry)) #length of string
+# print(harry.lower())
+# print(harry.upper())
+# print(harry.replace("r", "R"))
+# print(harry.split(' '))
+# sentence = "I like to watch first {1} then {0}".format(db,harry) #old process python version < 3.6
+# sentence = f"I like to watch first {db} then {harry}"
+# print(sentence)
+
+'''
+Python Collection
+1. List
+2. Tuple
+3. Set
+4. Dictionary
+'''
+
+py_list=[98,2,100,5,1,4,6,3]
+print(type(py_list))
+# print(py_list)
+# print(py_list[4])
+# print(py_list[2:6])
+# py_list[6] = 60  # it changes the original list
+# print(py_list)
+# print(len(py_list))
+# py_list.append(20) #add item at the end
+# py_list.insert(1,200) #add item at 1th index 
+# py_list.remove(2)  # rempve item from the list 
+# py_list.pop() # remove from end
+# py_list.pop(4)  # remove from index 4
+# del py_list[2] # delete 2th index element
+# print(py_list)
